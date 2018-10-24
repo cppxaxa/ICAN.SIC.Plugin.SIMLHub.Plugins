8b4a7542dc517d82fb5c160ee568223710348863
Christ Prateek Prasanna Xaxa <cppxaxa@live.com>
2018-10-24 22:04:03 +0530

    Automatic push

--- SIMLHub_Test/Program.cs
+++ SIMLHub_Test/Program.cs
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-﻿using ICAN.SIC.Abstractions.IMessageVariants;
-using ICAN.SIC.Plugin.SIMLHub;
-using Syn.Bot.Siml;
-using Syn.Bot.Siml.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SIMLHub_Test
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            SIMLHub simlHub = new SIMLHub();
-
-            Console.WriteLine("Adapter count: " + simlHub.AdapterCount);
-
-
-            simlHub.Hub.Publish<IUserResponse>(new UserResponse("Connect camera 0"));
-            //simlHub.Hub.Publish<IUserResponse>(new UserResponse("Open notepad"));
-
-            Console.WriteLine("Done");
-            Console.Read();
-        }
-    }
-}
-=======
 ﻿using ICAN.SIC.Abstractions.IMessageVariants;
 using ICAN.SIC.Plugin.SIMLHub;
 using System;
@@ -55,4 +23,3 @@ namespace SIMLHub_Test
         }
     }
 }
->>>>>>> 1be34f6468a452701526c1cffa489bf57f698bb0
